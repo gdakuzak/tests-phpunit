7525fc68f6810404881bca614d2e92279eef59c1
Guilherme Makoto Sacoman Dakuzaku <guilherme.dakuzaku@ciandt.com>
2022-06-22 08:15:14 -0300

    escopo de testes para produtos

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -5,17 +5,34 @@ use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase
 {
-    public function testSetName(){
+    public function testIfTotalIsNull() {
         $product = new Product();
-        $product->setName('Caju');
+        $this->assertNull($product->getTotal());
+    }
+
+    public function testIfIdNull() {
+        $product = new Product();
+        $this->assertNull($product->getId());
+    }
+
+    public function testSetAndGetName(){
+        $product = new Product();
+        $this->assertNull($product->getName());
+        $this->assertInstanceOf(Product::class,$product->setName('Caju'));
         $this->assertEquals('Caju',$product->getName());
     }
     
-    public function testSetPrice(){
+    public function testSetAndGetPrice(){
         $product = new Product();
-        $product->setPrice(10.10);
-        $this->assertEquals(10.10,$product->getPrice());
-        $this->assertInstanceOf(Product::class,$product);
+        $this->assertNull($product->getPrice());
+        $this->assertInstanceOf(Product::class,$product->setPrice(10.11));
+        $this->assertEquals(10.11,$product->getPrice());
     }
 
+    public function testSetAndGetQuantity(){
+        $product = new Product();
+        $this->assertNull($product->getQuantity());
+        $this->assertInstanceOf(Product::class,$product->setQuantity(150));
+        $this->assertEquals(150,$product->getQuantity());
+    }
 }
